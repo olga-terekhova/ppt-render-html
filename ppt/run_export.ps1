1faa60452b0d8639d35117d990fea8f99996da68
Olga Terekhova <97198539+olga-terekhova@users.noreply.github.com>
2025-04-10 16:31:26 -0400

    Edit for clarity

--- ppt/run_export.ps1
+++ ppt/run_export.ps1
@@ -9,8 +9,3 @@ $presentation = $application.Presentations.open($path)
 
 $presentation.application.Run("Save_to_canvas.pptm!ExportSlideWithLinkData") 
 $application.Quit()     
-
-#Popup box to show completion - you would remove this if using task scheduler 
-#$wshell = New-Object -ComObject Wscript.Shell $wshell.Popup("Operation Completed",0,"Done",0x1)  
-
-#exit 
